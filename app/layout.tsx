1ee89410e49e5b563d76579a7dcbe8a88cedf33f
Gianne Pesaña <gpesana@addu.edu.ph>
2026-04-28 19:23:16 +0800

    added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -31,6 +31,7 @@ export default function RootLayout({
     >
       <body 
         className="min-h-full flex flex-col bg-background text-foreground"
+        suppressHydrationWarning
       >
         <ThemeProvider
           attribute="class"
